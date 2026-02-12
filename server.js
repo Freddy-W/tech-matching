afa57b5d990c664467274f10ab3534c52f2641c6
ToriaKr07 <toria.kruithof@hva.nl>
2026-02-12 15:16:52 +0100

    commit

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// static files serveren
+app.use(express.static('static'));
+
+app.listen(port, () => {
+  console.log(`Server draait op http://localhost:${port}`);
+});
