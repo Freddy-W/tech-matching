2c12abeb85536a7ded33d4c947321878e25bb5a0
ToriaKr07 <toria.kruithof@hva.nl>
2026-03-16 13:38:08 +0100

    leeftijd balk

---
+++ static/scripts/script.js
@@ -0,0 +1,22 @@
+let sliderOne = document.getElementById("slider-1");
+let sliderTwo = document.getElementById("slider-2");
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2");
+let minGap = 5;
+
+function slideOne() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+        sliderOne.value = parseInt(sliderTwo.value) -minGap;
+    }
+    displayValOne.innerText = sliderOne.value;
+}
+
+function slideTwo() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+        sliderTwo.value = parseInt(sliderOne.value) + minGap;
+    }
+    displayValTwo.innerText = sliderTwo.value;
+}
+
+displayValOne.innerText = sliderOne.value;
+displayValTwo.innerText = sliderTwo.value;
