a7590da7046914b30f67d0176835ebe6e1585200
Geis <82882147+geispauw@users.noreply.github.com>
2026-03-16 14:42:29 +0100

    login/register werkend gemaakt.

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require("express");
+const mongoClient = require("mongodb");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+const bcrypt = require("bcryptjs");
+dotenv.config();
 const app = express();
 const port = 2020;
-
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// require("dotenv").config();const { MongoClient, ServerApiVersion } = require("mongodb");
-
-app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
 app.use(express.static("static"));
-
+app.set('view engine', 'ejs');
 app.listen(port, () => {
     console.log(`Server draait op http://localhost:${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/test', (req, res) => {
     res.send("De test is geslaagd, de URL /test is bereikbaar");
@@ -42,4 +39,62 @@ app.get('/events/:artist', async (req, res) => {
 
     }
 
+});
+
+app.get("/login", (req, res)=>{
+    res.render('login.ejs');
+});
+
+app.get("/register", (req, res)=>{
+    res.render('register.ejs');
+});
+
+mongoose.connect(process.env.dbPassword);
+
+const dataScheme = new mongoose.Schema({
+    email: String,
+    wachtwoord: String
+});
+
+const Data = mongoose.model("Data", dataScheme);
+app.post("/register", async (req, res) => {
+  try {
+    const loginData = {
+      email: req.body.email,
+      wachtwoord: req.body.wachtwoord
+    };
+
+    const existingUser = await Data.findOne({ email: loginData.email });
+    if (existingUser) return res.send("Email is already registered!");
+
+    const hashedPassword = await bcrypt.hash(loginData.wachtwoord, 10);
+    loginData.wachtwoord = hashedPassword;
+
+    await Data.create(loginData);
+    res.send("Registration successful!");
+  } catch (err) {
+    console.error(err);
+    res.send("Error registering user");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const loginData = {
+        email: req.body.email,
+        wachtwoord: req.body.wachtwoord
+    };
+
+    const user = await Data.findOne({ email: loginData.email });
+    if (!user) return res.send("Email not registered");
+
+    const match = await bcrypt.compare(loginData.wachtwoord, user.wachtwoord);
+    if (!match) return res.send("Incorrect wachtwoord");
+
+    res.send("Login successful!");
+
+  } catch (error) {
+    console.error(error);
+    res.send("Error logging in");
+  }
 });
