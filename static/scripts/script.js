0a1b54155f3bbb74e0e14b3c5d82b15702dd0df7
David Themans <david.themans@hva.nl>
2026-03-26 13:25:42 +0100

    SearchBtn functie weggehaald
    Stond er dubbel in waardoor de lijst met events niet goed weergegeven werd

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -84,26 +84,6 @@ document.getElementById("searchBtn").addEventListener("click", async () => {
 
 });
 
-const button = document.getElementById("searchBtn");
-
-document.getElementById("searchBtn").addEventListener("click", async () => {
-
-    const artist = document.getElementById("artistInput").value;
-
-    const response = await fetch(`http://localhost:2020/artist/${encodeURIComponent(artist)}`);
-
-    const data = await response.json();
-
-    console.log(data);
-
-    const results = document.getElementById("results");
-
-    results.innerHTML = 
-        `<h2>${data.name}</h2>
-        <p>Upcoming events: ${data.upcoming_event_count}</p>`;
-
-});
-
 
 // CONCERT OPSLAAN NIET COMPLEET
 const addConcertBtn = document.getElementById("...");
