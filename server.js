6b7c08c40e72437fc49bb8ed21857bd151d819bd
FreddyW <frederique.woord@hva.nl>
2026-03-31 16:25:32 +0200

    commit

--- server.js
+++ server.js
@@ -210,7 +210,6 @@ app.get("/gekozen-concert", (req, res)=>{
         country: req.query.country,
         image: req.query.image
         }
-        console.log(event)
         res.render('gekozen-concert.ejs', {event});
 });
 
